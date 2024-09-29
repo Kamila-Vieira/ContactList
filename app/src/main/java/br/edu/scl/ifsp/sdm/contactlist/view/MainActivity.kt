ed8ba512585c245e8ec712300024d8d204ccb393
Kamila Vieira <vkamila54@outlook.com>
2024-09-29 02:05:50 -0300

    chore: implementing contact update, remove and view
    project phase 2.1

--- app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/MainActivity.kt
@@ -3,14 +3,19 @@ package br.edu.scl.ifsp.sdm.contactlist.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView.AdapterContextMenuInfo
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import br.edu.scl.ifsp.sdm.contactlist.R
 import br.edu.scl.ifsp.sdm.contactlist.adapter.ContactAdapter
 import br.edu.scl.ifsp.sdm.contactlist.databinding.ActivityMainBinding
 import br.edu.scl.ifsp.sdm.contactlist.model.Constant.EXTRA_CONTACT
+import br.edu.scl.ifsp.sdm.contactlist.model.Constant.EXTRA_VIEW_CONTACT
 import br.edu.scl.ifsp.sdm.contactlist.model.Contact
 
 class MainActivity : AppCompatActivity() {
@@ -38,11 +43,12 @@ class MainActivity : AppCompatActivity() {
         carl = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
             if(result.resultCode == RESULT_OK){
                 val contact = result.data?.getParcelableExtra<Contact>(EXTRA_CONTACT)
-                contact?.also {
-                    if(contactList.any{ it.id == contact.id }){
-                        // TODO: Implementar edição
+                contact?.also { newOrEditedContact ->
+                    if(contactList.any{ it.id == newOrEditedContact.id }){
+                        val position = contactList.indexOfFirst{ it.id == newOrEditedContact.id }
+                        contactList[position] = newOrEditedContact
                     } else{
-                        contactList.add(contact)
+                        contactList.add(newOrEditedContact)
                     }
                     contactAdapter.notifyDataSetChanged()
                 }
@@ -51,6 +57,16 @@ class MainActivity : AppCompatActivity() {
 
         fillContacts()
         amb.contactsLv.adapter = contactAdapter
+
+        registerForContextMenu(amb.contactsLv)
+
+        amb.contactsLv.setOnItemClickListener{ _, _, position, _ ->
+            startActivity(Intent(this, ContactActivity::class.java).apply{
+                putExtra(EXTRA_CONTACT, contactList[position])
+                putExtra(EXTRA_VIEW_CONTACT, true)
+            })
+
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -68,10 +84,53 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        menuInflater.inflate(R.menu.context_menu_main, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        val position = (item.menuInfo as AdapterContextMenuInfo).position
+
+        return when(item.itemId){
+            R.id.removeContactMi -> {
+                onRemoveContact(position)
+                true
+            }
+            R.id.editContactMi -> {
+                onEditContact(position)
+                true
+            }
+            else -> { false }
+        }
+    }
+
+    private fun onRemoveContact(position: Int){
+        contactList.removeAt(position)
+        contactAdapter.notifyDataSetChanged()
+        Toast.makeText(this, getString(R.string.contact_removed), Toast.LENGTH_SHORT).show()
+    }
+
+    private fun onEditContact(position: Int){
+        val contact = contactList[position]
+        carl.launch(Intent(this, ContactActivity::class.java).apply {
+            putExtra(EXTRA_CONTACT, contact)
+        })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterForContextMenu(amb.contactsLv)
+    }
+
     private fun fillContacts(){
         for (i in 1..10){
             contactList.add(
                 Contact(
+                    id = i,
                     name = "Name $i",
                     address = "Endereço $i",
                     email = "Email $i",
